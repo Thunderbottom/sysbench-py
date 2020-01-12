e695498b982d85fc3d018b6ba8602bc7cd9dc1ce
Chinmay D. Pai <chinmaydpai@gmail.com>
2020-01-12 19:02:40 +0530

    chore: save example output to a timestamped logfile
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from backend.commands import Benchmark
 from configparser import ConfigParser
 
 import json
+import time
 
 
 def main():
@@ -9,7 +10,13 @@ def main():
     config.read("config.ini")
     hosts = json.loads(config.get("system", "hosts"))
     benchmark = Benchmark(config, hosts)
-    benchmark.run_all()
+    output = benchmark.run_all()
+
+    logfile = f"sysbench_{time.strftime('%Y%m%d-%H%M%S')}.log"
+    print(f"Saving execution log to: {logfile}")
+    with open(logfile, "w") as log:
+        log.write(json.dumps(output, indent=4, sort_keys=True))
+        log.flush()
 
 
 if __name__ == "__main__":
